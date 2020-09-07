c46127dba0011f0aacc2e2caa20a81aade0b3bf3
Alfarell Muchamad Yuwanto <malfarell@gmail.com>
2020-09-07 15:02:21 +0700

    [DELETE] delete unused queries

--- q.js
+++ q.js
@@ -94,7 +94,7 @@
 `
 
 {
-    query: `query(
+  query: `query(
         $page:Int = 1 
         $id:Int 
         $type:MediaType 
@@ -133,92 +133,92 @@
                     hasNextPage
                 }
                 media(
-                    id:$id 
-                    type:$type 
-                    season:$season 
-                    format_in:$format 
-                    status:$status 
-                    countryOfOrigin:$countryOfOrigin 
-                    source:$source 
-                    search:$search 
-                    onList:$onList 
-                    startDate_like:$year 
-                    startDate_lesser:$yearLesser 
-                    startDate_greater:$yearGreater 
-                    episodes_lesser:$episodeLesser 
-                    episodes_greater:$episodeGreater 
-                    duration_lesser:$durationLesser 
-                    duration_greater:$durationGreater 
-                    chapters_lesser:$chapterLesser 
-                    chapters_greater:$chapterGreater 
-                    volumes_lesser:$volumeLesser 
-                    volumes_greater:$volumeGreater 
-                    licensedBy_in:$licensedBy 
-                    genre_in:$genres 
-                    genre_not_in:$excludedGenres 
-                    tag_in:$tags 
-                    tag_not_in:$excludedTags 
-                    minimumTagRank:$minimumTagRank 
-                    sort:$sort 
-                    isAdult:$isAdult){
-                        id 
-                        title{
-                            userPreferred
-                        }
-                        coverImage{
-                            extraLarge 
-                            large 
-                            color
-                        }
-                        startDate{
-                            year 
-                            month 
-                            day
-                        }
-                        endDate{
-                            year 
-                            month 
-                            day
-                        }
-                        bannerImage 
-                        season 
-                        description 
-                        type 
-                        format 
-                        status 
-                        episodes 
-                        duration 
-                        chapters 
-                        volumes 
-                        genres 
-                        isAdult 
-                        averageScore 
-                        popularity 
-                        nextAiringEpisode{
-                            airingAt 
-                            timeUntilAiring 
-                            episode
-                        }
-                        mediaListEntry{
-                            id status
-                        }
-                        studios(isMain:true){
-                            edges{
-                                isMain 
-                                node{
-                                    id 
-                                    name
-                                }
-                            }
-                        }
+                  id:$id 
+                  type:$type 
+                  season:$season 
+                  format_in:$format 
+                  status:$status 
+                  countryOfOrigin:$countryOfOrigin 
+                  source:$source 
+                  search:$search 
+                  onList:$onList 
+                  startDate_like:$year 
+                  startDate_lesser:$yearLesser 
+                  startDate_greater:$yearGreater 
+                  episodes_lesser:$episodeLesser 
+                  episodes_greater:$episodeGreater 
+                  duration_lesser:$durationLesser 
+                  duration_greater:$durationGreater 
+                  chapters_lesser:$chapterLesser 
+                  chapters_greater:$chapterGreater 
+                  volumes_lesser:$volumeLesser 
+                  volumes_greater:$volumeGreater 
+                  licensedBy_in:$licensedBy 
+                  genre_in:$genres 
+                  genre_not_in:$excludedGenres 
+                  tag_in:$tags 
+                  tag_not_in:$excludedTags 
+                  minimumTagRank:$minimumTagRank 
+                  sort:$sort 
+                  isAdult:$isAdult){
+                    id 
+                    title{
+                        userPreferred
                     }
+                    coverImage{
+                        extraLarge 
+                        large 
+                        color
+                    }
+                    startDate{
+                        year 
+                        month 
+                        day
+                    }
+                    endDate{
+                        year 
+                        month 
+                        day
+                    }
+                    bannerImage 
+                    season 
+                    description 
+                    type 
+                    format 
+                    status 
+                    episodes 
+                    duration 
+                    chapters 
+                    volumes 
+                    genres 
+                    isAdult 
+                    averageScore 
+                    popularity 
+                    nextAiringEpisode{
+                        airingAt 
+                        timeUntilAiring 
+                        episode
+                    }
+                    mediaListEntry{
+                        id status
+                    }
+                    studios(isMain:true){
+                      edges{
+                        isMain 
+                        node{
+                          id 
+                          name
+                        }
+                      }
+                    }
+                  }
                 }
             }`;
-    variables: {
-        page: 1;
-        type: "ANIME";
-        sort: "SCORE_DESC"
-    }
+  variables: {
+    page: 1;
+    type: "ANIME";
+    sort: "SCORE_DESC"
+  }
 }
 
 
