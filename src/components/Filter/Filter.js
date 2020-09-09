77fe1546994e731adf86e184a434dbacd519e53d
Alfarell Muchamad Yuwanto <malfarell@gmail.com>
2020-09-09 21:41:07 +0700

    [UPDATE] add genre and tag collection data

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,5 +1,16 @@
 import React from 'react'
-import { Paper, List, ListSubheader, makeStyles } from '@material-ui/core'
+import {
+  Paper,
+  List,
+  ListSubheader,
+  makeStyles,
+} from '@material-ui/core'
+import { graphql } from '@apollo/react-hoc'
+import { flowRight as compose } from 'lodash'
+import { GENRE_LIST_QUERY, TAG_LIST_QUERY } from '../../settings/graphql/queries/FilterQueries'
+import { useLocation } from 'react-router-dom'
+import getURLQuery from '../../utils/GetQuery'
+import FilterSubMenu from './FilterSubMenu'
 
 
 const useStyles = makeStyles(theme => ({
@@ -7,11 +18,14 @@ const useStyles = makeStyles(theme => ({
     [theme.breakpoints.up('md')]: {
       width: 200
     }
-  }
+  },
 }))
 
-const Filter = () => {
+const Filter = ({ GetGenreList, GetTagList }) => {
   const classes = useStyles()
+  const location = useLocation()
+  const query = getURLQuery(location.search)
+  console.log(query.getAll('genres'));
 
   return (
     <Paper className={classes.container}>
@@ -21,13 +35,21 @@ const Filter = () => {
         subheader={
           <ListSubheader component="div" id="nested-list-subheader">
             Filter
-                </ListSubheader>
+          </ListSubheader>
         }
       >
-
+        <FilterSubMenu genreList={GetGenreList} title='Genres' />
+        <FilterSubMenu tagList={GetTagList} title='Tags' />
       </List>
     </Paper>
   )
 }
 
-export default Filter
+export default compose(
+  graphql(GENRE_LIST_QUERY, {
+    name: 'GetGenreList'
+  }),
+  graphql(TAG_LIST_QUERY, {
+    name: 'GetTagList'
+  }),
+)(Filter)
